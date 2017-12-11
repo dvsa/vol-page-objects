948794e9e69beccb1f1cf0d2159bd7fc185a8f55
Rodney Ruswa <rodney.ruswa@bjss.com>
2017-12-11 10:30:45 +0000

    Fix unit tests in LoginPageTest

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,31 +1,52 @@
-package external;
-
+import activesupport.system.Properties;
 import org.dvsa.testing.lib.Browser;
+import org.dvsa.testing.lib.Environment;
 import org.dvsa.testing.lib.URI;
 import org.dvsa.testing.lib.utils.ApplicationType;
-import org.dvsa.testing.lib.utils.Environment;
+import org.dvsa.testing.lib.utils.EnvironmentType;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class LoginPageTest {
-    private static String url = URI.build(ApplicationType.EXTERNAL, Environment.DAILY_ASSURANCE, "auth/login/");
+import java.io.FileNotFoundException;
 
-    @Before
-    public void setUp(){
-        Browser.open(url);
+public class LoginPageTest {
+    String endPoint = "auth/login/";
+
+    @BeforeClass
+    public static void beforeAll() throws FileNotFoundException {
+        Properties.loadConfigPropertiesFromFile();
+
+        if(System.getProperty("browser") == null){
+            System.setProperty("browser", "chrome");
+        }
+        if(System.getProperty("env") == null){
+            System.setProperty("env", "da");
+        }
     }
 
     @Test
-    public void goToLogonPage(){
-        Browser.open(url);
-        Assert.assertEquals(url, Browser.getURL());
+    public void goToInternalLogonPage() {
+        EnvironmentType environmentType = Environment.enumType(System.getProperty("env"));
+        String URL = URI.build(ApplicationType.INTERNAL, environmentType, endPoint);
+
+        Browser.open(URL);
+        Assert.assertEquals(URL, Browser.getURL());
+    }
+
+    @Test
+    public void goToExternalLogonPage(){
+        EnvironmentType environmentType = Environment.enumType(System.getProperty("env"));
+        String URL = URI.build(ApplicationType.EXTERNAL, environmentType, endPoint);
+
+        Browser.open(URL);
+        Assert.assertEquals(URL, Browser.getURL());
     }
 
     @After
     public void tearDown(){
-        Browser.close();
+        Browser.quit();
     }
 
 }
