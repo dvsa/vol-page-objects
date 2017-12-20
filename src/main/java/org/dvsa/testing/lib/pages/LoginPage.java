573d3dceaffb66bccfd0ff43750879515433109d
Rodney Ruswa <rodney.ruswa@bjss.com>
2017-12-20 10:48:22 +0000

    Delete LoginPage#signin and up POM from 0.3.5 to 0.3.6

--- src/main/java/org/dvsa/testing/lib/pages/LoginPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/LoginPage.java
@@ -6,28 +6,18 @@ public class LoginPage extends BasePage {
 
     private static String EMAIL_FIELD_LOCATOR = createNameAttributeSelector("input", "[name=\"username\"]");
     private static String PASSWORD_FIELD_LOCATOR = createNameAttributeSelector("input", "[name=\"password\"]");;
-    private static String SUBMIT_BUTTON = createNameAttributeSelector("input", "[name=\"submit\"]");;
+    private static String SUBMIT_BUTTON = createNameAttributeSelector("input", "[name=\"submit\"]");
 
-    public static void signin(@NotNull String email, @NotNull String password){
-        email(email);
-        password(password);
-        click(SUBMIT_BUTTON);
-    }
-
-    private static void email(@NotNull String email) {
+    public static void email(@NotNull String email) {
         enterField(EMAIL_FIELD_LOCATOR, email);
     }
 
-    private static void password(@NotNull String password) {
+    public static void password(@NotNull String password) {
         enterField(PASSWORD_FIELD_LOCATOR, password);
     }
 
-    public static String getEmailFieldLocator() {
-        return EMAIL_FIELD_LOCATOR;
-    }
-
-    public static String getPasswordFieldLocator() {
-        return PASSWORD_FIELD_LOCATOR;
+    public static void submit(){
+        click(SUBMIT_BUTTON);
     }
 
 }
