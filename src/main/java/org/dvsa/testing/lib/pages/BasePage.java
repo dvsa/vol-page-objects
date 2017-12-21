593bc97ec9b5aa53a823a510a36b07d9bca65280
Rodney Ruswa <rodney.ruswa@bjss.com>
2017-12-21 08:46:12 +0000

    Fix cannot find symbol errors caused by changing BasePage method  access modifiers to package private

--- src/main/java/org/dvsa/testing/lib/pages/BasePage.java
+++ src/main/java/org/dvsa/testing/lib/pages/BasePage.java
@@ -9,23 +9,23 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
-    static void enterField(@NotNull String selector, @NotNull String text){
+    protected static void enterField(@NotNull String selector, @NotNull String text){
         find(selector).sendKeys(text);
     }
 
-    static void click(@NotNull String selector){
+    protected static void click(@NotNull String selector){
        find(selector).click();
     }
 
-    static WebDriver getDriver(){
+    protected static WebDriver getDriver(){
         return Browser.getDriver();
     }
 
-    static String createNameAttributeSelector(@NotNull String element, @NotNull String value){
+    protected static String createNameAttributeSelector(@NotNull String element, @NotNull String value){
         return createAttributeSelector(element, "name", value);
     }
 
-    static String createAttributeSelector(@NotNull String element, @NotNull String attribute, @NotNull String value){
+    protected static String createAttributeSelector(@NotNull String element, @NotNull String attribute, @NotNull String value){
         return String.format("%s[%s=\"%s\"]", element, attribute, value);
     }
 
@@ -33,35 +33,35 @@ public class BasePage {
         return Browser.getDriver().findElement(By.cssSelector(selector));
     }
 
-    static boolean isElementPresent(@NotNull String selector){
+    protected static boolean isElementPresent(@NotNull String selector){
         return getDriver().findElement(By.cssSelector(selector)) != null;
     }
 
-    static boolean isNotSelected(@NotNull String selector){
+    protected static boolean isNotSelected(@NotNull String selector){
         return !isSelected(selector);
     }
 
-    static boolean isSelected(@NotNull String selector){
+    protected static boolean isSelected(@NotNull String selector){
         return getDriver().findElement(By.cssSelector(selector)).isSelected();
     }
 
-    static void select(@NotNull String selector){
+    protected static void select(@NotNull String selector){
         if(isNotSelected(selector)){
             click(selector);
         }
     }
 
-    static void deselect(@NotNull String selector){
+    protected static void deselect(@NotNull String selector){
         if(isSelected(selector)){
             click(selector);
         }
     }
 
-    static void isNotPresent(@NotNull String selector, int timeToWait){
+    protected static void isNotPresent(@NotNull String selector, int timeToWait){
         new WebDriverWait(getDriver(), timeToWait).until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(selector)));
     }
 
-    static void isNotInDOM(@NotNull String selector, int timeToWait){
+    protected static void isNotInDOM(@NotNull String selector, int timeToWait){
         new WebDriverWait(getDriver(), timeToWait).until(BasePage.absenceOfElementLocated(By.cssSelector(selector)));
     }
 
