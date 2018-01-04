c8ab578e7287392d42d6f4cfe46998a269b17984
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-01-04 13:50:02 +0000

    Add LoginPage#untilNotOnPage to wait until a unique element is no longer present so that callers know that the page has chaged to something else

--- src/main/java/org/dvsa/testing/lib/pages/LoginPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/LoginPage.java
@@ -6,7 +6,7 @@ public class LoginPage extends BasePage {
 
     private static String EMAIL_FIELD_LOCATOR = nameAttribute("input", "username");
     private static String PASSWORD_FIELD_LOCATOR = nameAttribute("input", "password");
-    private static String SUBMIT_BUTTON = nameAttribute("input", "submit");
+    private static String SUBMIT_BUTTON = nameAttribute("input", "submit") + "[value=\"Sign in\"]";
 
     public static void email(@NotNull String email) {
         enterField(EMAIL_FIELD_LOCATOR, email);
@@ -18,6 +18,15 @@ public class LoginPage extends BasePage {
 
     public static void submit(){
         click(SUBMIT_BUTTON);
+    }
+
+    public static void untilNotOnPage(){
+        int seconds = 30;
+        untilNotOnPage(seconds);
+    }
+
+    public static void untilNotOnPage(int seconds){
+        isNotPresent(SUBMIT_BUTTON, seconds);
     }
 
 }
