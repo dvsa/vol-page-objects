51ec1337134a40eedb04cc61d9c20eba0abc442d
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-01-04 10:03:26 +0000

    Create GangeYourPassword page object

---
+++ src/main/java/org/dvsa/testing/lib/pages/external/ChangeYourPassword.java
@@ -0,0 +1,27 @@
+package org.dvsa.testing.lib.pages.external;
+
+import org.dvsa.testing.lib.pages.BasePage;
+import org.jetbrains.annotations.NotNull;
+
+public class ChangeYourPassword extends BasePage{
+    private static String CURRENT_PASSWORD_FIELD = nameAttribute("input", "oldPassword");
+    private static String NEW_PASSWORD_FIELD = nameAttribute("input", "newPassword");
+    private static String CONFIRM_PASSWORD_FIELD = nameAttribute("input", "confirmPassword");
+    private static String SUBMIT_BUTTON = nameAttribute("input", "submit");
+
+    public void setCurrentPasswordField(@NotNull String currentPasswordField){
+        enterField(CURRENT_PASSWORD_FIELD, currentPasswordField);
+    }
+
+    public void setNewPasswordField(@NotNull String newPasswordField){
+        enterField(NEW_PASSWORD_FIELD, newPasswordField);
+    }
+
+    public void setConfirmPasswordField(@NotNull String confirmPasswordField){
+        enterField(CONFIRM_PASSWORD_FIELD, confirmPasswordField);
+    }
+
+    public void submit(){
+        click(SUBMIT_BUTTON);
+    }
+}
