c4e8e55adda60d1785f1aca5f16976e846c8a99c
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-01-05 13:48:29 +0000

    Remove unused import statements from BasePage

--- src/main/java/org/dvsa/testing/lib/pages/BasePage.java
+++ src/main/java/org/dvsa/testing/lib/pages/BasePage.java
@@ -2,8 +2,9 @@ package org.dvsa.testing.lib.pages;
 
 import org.dvsa.testing.lib.Browser;
 import org.jetbrains.annotations.NotNull;
-import org.openqa.selenium.*;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
