8b4e63dc3a33e60b579c87c9ce0e717651d6e498
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-01-19 13:20:30 +0000

    Add description to the DataRetentionRule enum.

--- src/main/java/org/dvsa/testing/lib/pages/enums/DataRetentionRule.java
+++ src/main/java/org/dvsa/testing/lib/pages/enums/DataRetentionRule.java
@@ -1,5 +1,8 @@
 package org.dvsa.testing.lib.pages.enums;
 
+/**
+ * This enum represents the options available under the rule admin tab on data retention.
+ */
 public enum DataRetentionRule {
     TRANSPORT_MANAGERS_WITH_PUBLIC_INQUIRYS ("Transport Managers With Public Inquirys"),
     LICENCE_WITH_PI_NO_IMPOUNDING_AND_NOT_UNLICENCED ("Licence With Pi, No Impounding And Not Unlicenced"),
