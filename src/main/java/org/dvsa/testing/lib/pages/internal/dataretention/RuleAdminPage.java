2a72b688b64396a0f5a55c783e8b8e7a539f97ff
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-01-22 11:18:37 +0000

    Add documentation to RuleAdminPage#selectRule and fix a minor bug were it wouldn't go to the next pagination if the rule was not present on the page. An exception also now gets thrown should it paginate through all the pages and not find the specified rule.

--- src/main/java/org/dvsa/testing/lib/pages/internal/dataretention/RuleAdminPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/internal/dataretention/RuleAdminPage.java
@@ -3,6 +3,7 @@ package org.dvsa.testing.lib.pages.internal.dataretention;
 import org.dvsa.testing.lib.browser.exceptions.UninitialisedDriverException;
 import org.dvsa.testing.lib.pages.enums.DataRetentionRule;
 import org.dvsa.testing.lib.pages.enums.SelectorType;
+import org.dvsa.testing.lib.pages.exception.UnableToFindDataRetentionRule;
 import org.jetbrains.annotations.NotNull;
 
 public class RuleAdminPage extends DataRetentionPage {
@@ -17,14 +18,32 @@ public class RuleAdminPage extends DataRetentionPage {
 
     }
 
-    public static void selectRule(@NotNull DataRetentionRule dataRetentionRule) throws UninitialisedDriverException {
+    /**
+     * Finds and selects the specified data retention rule. This method will go through the various pagination while
+     * trying to find the data retention rule.
+     * @param dataRetentionRule This is the that the user wishes to select.
+     * @throws UninitialisedDriverException is thrown if the driver is null, hasn't been initialised,  or has been closed
+     * @throws UnableToFindDataRetentionRule is thrown if the specified rule is not present in any of the pagination.
+     */
+    public static void selectRule(@NotNull DataRetentionRule dataRetentionRule) throws UninitialisedDriverException, UnableToFindDataRetentionRule {
         String ruleSelector = String.format(RULE_DESCRIPTION_ROW_TEMPLATE, dataRetentionRule);
+        boolean foundDataRetentionRule = false;
 
         do {
             if (isElementPresent(ruleSelector, SelectorType.XPATH)) {
+                foundDataRetentionRule = true;
                 click(ruleSelector, SelectorType.XPATH);
+            } else {
+                click(NEXT_BUTTON);
             }
-        } while (!isElementPresent(ruleSelector, SelectorType.XPATH) && );
+        } while (!isElementPresent(ruleSelector, SelectorType.XPATH) && isElementPresent(NEXT_BUTTON, SelectorType.XPATH));
+
+        if (!foundDataRetentionRule) {
+            throw new UnableToFindDataRetentionRule("Unable to find the data retention rule '"
+                    + dataRetentionRule.name()
+                    + "' from the Rule Admin page (including pagination)"
+            );
+        }
     }
 
     public static String getActionType(@NotNull DataRetentionRule dataRetentionRule) throws UninitialisedDriverException {
