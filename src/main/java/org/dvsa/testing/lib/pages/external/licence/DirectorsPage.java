43506bccbf3057cdc0145b3176dc9d75f71b3161
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-06 13:36:05 +0000

    Create Directors page object

---
+++ src/main/java/org/dvsa/testing/lib/pages/external/licence/DirectorsPage.java
@@ -0,0 +1,30 @@
+package org.dvsa.testing.lib.pages.external.licence;
+
+import org.dvsa.testing.lib.browser.exceptions.UninitialisedDriverException;
+import org.dvsa.testing.lib.pages.BasePage;
+import org.dvsa.testing.lib.pages.enums.SelectorType;
+
+public class DirectorsPage extends BasePage {
+    // Selectors
+    private static String ADD_DIRECTOR_BUTTON = "button[name='table[action]']";
+
+    private static String BUTTON_WITH_TEXT_TEMPLATE = "//*/button[contains(text(), '%s')]";
+    private static String SAVE_AND_CONTINUE_BUTTON = String.format(BUTTON_WITH_TEXT_TEMPLATE, "Save and continue");
+    private static String SAVE_AND_RETURN_TO_OVERVIEW = String.format(BUTTON_WITH_TEXT_TEMPLATE, "Save and return to overview");
+    // Attributes
+    private static String PAGE_TITLE_TEXT = "Directors";
+    private static String RESOURCE_PATH = "application/\\d{7}/people/";
+
+    // Behaviour
+    public static void addDirectorButton() throws UninitialisedDriverException {
+        click(ADD_DIRECTOR_BUTTON);
+    }
+
+    public static void saveAndContinueButton() throws UninitialisedDriverException {
+        click(SAVE_AND_CONTINUE_BUTTON, SelectorType.XPATH);
+    }
+
+    public static void saveAndReturnToOverview() throws UninitialisedDriverException {
+        click(SAVE_AND_RETURN_TO_OVERVIEW, SelectorType.XPATH);
+    }
+}
---
+++ src/main/java/org/dvsa/testing/lib/pages/external/licence/enums/Title.java
@@ -0,0 +1,10 @@
+package org.dvsa.testing.lib.pages.external.licence.enums;
+
+public enum Title {
+    PLEASE_SELECT,
+    DR,
+    MISS,
+    MR,
+    MRS,
+    MS;
+}
