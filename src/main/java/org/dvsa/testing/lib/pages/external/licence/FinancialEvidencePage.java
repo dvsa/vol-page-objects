4a59e9de501021823a763aa07e83059795e3feab
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-07 13:26:38 +0000

    Add to financial evidence page object methods for clicking the saving and continue button and save and return to overview button

--- src/main/java/org/dvsa/testing/lib/pages/external/licence/FinancialEvidencePage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/licence/FinancialEvidencePage.java
@@ -10,8 +10,19 @@ public class FinancialEvidencePage extends BasePage {
     // Selectors
     private static String DELIVERY_METHOD_TEMPLATE = "(//*/input[@type='radio'])[%d]";
 
+    private static String SAVE_AND_CONTINUE_BUTTON = "button[name='form-actions[saveAndContinue]']";
+    private static String SAVE_AND_RETURN_TO_OVERVIEW_BUTTON = "button[name='form-actions[saveAndContinue]']";
+
     // Behaviour
     public static void submitEvidence(@NotNull DeliveryMethod deliveryMethod) throws UninitialisedDriverException {
         click(String.format(DELIVERY_METHOD_TEMPLATE, deliveryMethod.ordinal() + 1), SelectorType.XPATH);
+    }
+
+    public static void saveAndContinue() throws UninitialisedDriverException {
+        click(SAVE_AND_CONTINUE_BUTTON);
+    }
+
+    public static void saveAndReturnToOverview() throws UninitialisedDriverException {
+        click(SAVE_AND_RETURN_TO_OVERVIEW_BUTTON);
     }
 }
