fcbb106f4c606e43af378ecc8208d50cab664dce
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-16 13:45:26 +0000

    Remove unnecessary whitespace from ApplyForNewLicencePage class

--- src/main/java/org/dvsa/testing/lib/pages/external/licence/ApplyForNewLicencePage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/licence/ApplyForNewLicencePage.java
@@ -8,7 +8,6 @@ public class ApplyForNewLicencePage extends BasePage {
     // Selectors
     private static String OVERVIEW_LIST_ITEM_TEMPLATE = ".overview__list li:nth-of-type(%d)";
 
-
     // Attribute
     private static String PAGE_TITLE_TEXT = "Apply for a new licence";
     private static String RESOURCE_PATH = "application/\\d{7}/";
