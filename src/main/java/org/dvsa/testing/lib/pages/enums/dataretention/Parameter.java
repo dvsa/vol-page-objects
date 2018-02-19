191e78274a0e6e5e149facba9d3742faf77ddd37
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-19 14:03:45 +0000

    Rename the enum SystemParameter to Parameter as the system part of the name is made redundent by the package name that the enum is in

---
+++ src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/Parameter.java
@@ -0,0 +1,8 @@
+package org.dvsa.testing.lib.pages.enums.dataretention;
+
+public enum Parameter {
+    DISABLE_DATA_RETENTION_DELETE,
+    DISABLE_DATA_RETENTION_RECORDS,
+    DR_DELETE_LIMIT,
+    DISABLE_DR_DOCUMENT_DELETE;
+}
--- src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/SystemParameter.java
+++
@@ -1,6 +0,0 @@
-package org.dvsa.testing.lib.pages.enums.dataretention;
-
-public enum SystemParameter {
-    DISABLE_DATA_RETENTION_DELETE,
-    DISABLE_DATA_RETENTION_RECORDS
-}
