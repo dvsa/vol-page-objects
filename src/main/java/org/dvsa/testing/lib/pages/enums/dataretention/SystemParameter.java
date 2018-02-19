5a84a19be56413216b5f13dc6454f65aa6aba95a
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-19 14:42:29 +0000

    Add to the SystemParameter enum the values needed by each rule to enable and disable it

--- src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/SystemParameter.java
+++ src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/SystemParameter.java
@@ -5,4 +5,26 @@ public enum SystemParameter {
     DISABLE_DATA_RETENTION_RECORDS,
     DR_DELETE_LIMIT,
     DISABLE_DR_DOCUMENT_DELETE;
+
+    private String enableValue;
+    private String disableValue;
+
+    static {
+        DISABLE_DATA_RETENTION_DELETE.enableValue = "0";
+        DISABLE_DATA_RETENTION_RECORDS.enableValue = "0";
+        DISABLE_DR_DOCUMENT_DELETE.enableValue = "0";
+
+        DISABLE_DATA_RETENTION_DELETE.enableValue = "1";
+        DISABLE_DATA_RETENTION_RECORDS.enableValue = "1";
+        DISABLE_DR_DOCUMENT_DELETE.enableValue = "1";
+    }
+
+    public String getEnableValue() {
+        return this.enableValue;
+    }
+
+    public String getDisableValue() {
+        return this.disableValue;
+    }
+
 }
