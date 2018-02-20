df3150adc41951f9c6d52bf9cf9088e425d08c05
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-20 08:49:00 +0000

    Rewrite SystemParameter enum to remove potential confusion due to how enabling most system parameters would actually disable them

--- src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/SystemParameter.java
+++ src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/SystemParameter.java
@@ -1,22 +1,29 @@
 package org.dvsa.testing.lib.pages.enums.dataretention;
 
-public enum SystemParameter {
-    DISABLE_DATA_RETENTION_DELETE,
-    DISABLE_DATA_RETENTION_RECORDS,
-    DR_DELETE_LIMIT,
-    DISABLE_DR_DOCUMENT_DELETE;
+import org.jetbrains.annotations.NotNull;
 
+public enum SystemParameter {
+    DATA_RETENTION_DELETE("DISABLE_DATA_RETENTION_DELETE"),
+    DATA_RETENTION_RECORDS("DISABLE_DATA_RETENTION_RECORDS"),
+    DR_DELETE_LIMIT("DR_DELETE_LIMIT"),
+    DR_DOCUMENT_DELETE("DISABLE_DR_DOCUMENT_DELETE");
+
+    private String name;
     private String enableValue;
     private String disableValue;
 
-    static {
-        DISABLE_DATA_RETENTION_DELETE.enableValue = "0";
-        DISABLE_DATA_RETENTION_RECORDS.enableValue = "0";
-        DISABLE_DR_DOCUMENT_DELETE.enableValue = "0";
+    SystemParameter(@NotNull String name) {
+        this.name = name;
+    }
 
-        DISABLE_DATA_RETENTION_DELETE.enableValue = "1";
-        DISABLE_DATA_RETENTION_RECORDS.enableValue = "1";
-        DISABLE_DR_DOCUMENT_DELETE.enableValue = "1";
+    static {
+        DATA_RETENTION_DELETE.enableValue = "0";
+        DATA_RETENTION_RECORDS.enableValue = "0";
+        DR_DOCUMENT_DELETE.enableValue = "0";
+
+        DATA_RETENTION_DELETE.disableValue = "1";
+        DATA_RETENTION_RECORDS.disableValue = "1";
+        DR_DOCUMENT_DELETE.disableValue = "1";
     }
 
     public String getEnableValue() {
