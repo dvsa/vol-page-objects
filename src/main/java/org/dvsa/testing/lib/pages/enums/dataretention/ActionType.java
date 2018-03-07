b662664f20e8555a71c98d501d0257dc5a902783
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-03-07 07:30:59 +0000

    Add member ActionType#getEnum which resolves strings whose value is review or automate to their equivalent enum constants

--- src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/ActionType.java
+++ src/main/java/org/dvsa/testing/lib/pages/enums/dataretention/ActionType.java
@@ -1,6 +1,31 @@
 package org.dvsa.testing.lib.pages.enums.dataretention;
 
+import activesupport.system.out.Output;
+import org.jetbrains.annotations.NotNull;
+
 public enum ActionType {
+
     REVIEW,
-    AUTOMATE
+    AUTOMATE;
+
+    public static ActionType getEnum(@NotNull String actionType) {
+        ActionType action;
+
+        switch (actionType.toLowerCase()) {
+            case "automate":
+                action = ActionType.AUTOMATE;
+                break;
+            case "review":
+                action = ActionType.REVIEW;
+                break;
+            default:
+                throw new IllegalArgumentException(Output.printColoredLog(String.format(
+                        "[ERROR] Unsupported NAME: %S passed as an argument.",
+                        actionType
+                )));
+        }
+
+        return action;
+    }
+
 }
