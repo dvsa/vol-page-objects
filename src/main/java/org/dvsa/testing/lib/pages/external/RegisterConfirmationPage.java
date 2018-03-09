05280558b10f1cd77506fc28e792ddd0312aa384
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-03-09 16:24:45 +0000

    taking strings out of methods and storing them in fields

--- src/main/java/org/dvsa/testing/lib/pages/external/RegisterConfirmationPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/RegisterConfirmationPage.java
@@ -9,6 +9,8 @@ import java.util.regex.Pattern;
 public class RegisterConfirmationPage extends BasePage {
     private static String CONFIRM_EMAIL_TEMPPASS_SENT_MESSAGE = ".guidance p";
     private static String EMAIL_NOT_RECIVEVED_MESSAGE = ".article p:nth-of-type(2)";
+    private static String CHECK_EMAIL = "We have sent an email to .+\\@.+\\..+ containing a temporary password. Once you’ve signed in using the temporary password you will need to create a new password";
+    private static String SIGNING_IN_PROBLEMS = "If our email does not appear in your inbox within 5 minutes, check your Spam or Junk folder or contact your IT department for help. If our email is in your Spam or Junk folder, please change your email settings so that further emails from us are directed to your inbox. If you have issues signing in or didn’t receive our email you will need to contact notifications@vehicle-operator-licensing.service.gov.uk.";
 
 
     public static String getConfirmEmailTemppassSentMessageText() throws UninitialisedDriverException {
@@ -22,11 +24,11 @@ public class RegisterConfirmationPage extends BasePage {
     //Attributes
     public static Pattern checkEmail() {
         return
-                Pattern.compile("We have sent an email to .+\\@.+\\..+ containing a temporary password. Once you’ve signed in using the temporary password you will need to create a new password");
+                Pattern.compile(CHECK_EMAIL);
     }
 
     public static String signingInProblems() {
-        return "If our email does not appear in your inbox within 5 minutes, check your Spam or Junk folder or contact your IT department for help. If our email is in your Spam or Junk folder, please change your email settings so that further emails from us are directed to your inbox. If you have issues signing in or didn’t receive our email you will need to contact notifications@vehicle-operator-licensing.service.gov.uk.";
+        return SIGNING_IN_PROBLEMS;
     }
 
 }
