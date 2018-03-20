08e1eb73a4fb9202d3850466d56da6beb2254663
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-03-20 15:54:05 +0000

    Make RegisterConfirmationPage#checkEmail return a string instead of a pattern leave it to the callers of this method to decide if they need to create pattern as some regex methods accept a string

--- src/main/java/org/dvsa/testing/lib/pages/external/RegisterConfirmationPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/RegisterConfirmationPage.java
@@ -10,7 +10,7 @@ public class RegisterConfirmationPage extends BasePage {
     private static String EMAIL_NOT_RECIVEVED_MESSAGE = ".article p:nth-of-type(2)";
 
     //Attributes
-    private static String CHECK_EMAIL = "We have sent an email to .+\\@.+\\..+ containing a temporary password. Once you've signed in using the temporary password you will need to create a new password";
+    private static String CHECK_EMAIL = "We have sent an email to .+\\@.+\\..+ containing a temporary password. Once you.ve signed in using the temporary password you will need to create a new password";
     private static String SIGNING_IN_PROBLEMS = "If our email does not appear in your inbox within 5 minutes, check your Spam or Junk folder or contact your IT department for help. If our email is in your Spam or Junk folder, please change your email settings so that further emails from us are directed to your inbox. If you have issues signing in or didn’t receive our email you will need to contact notifications@vehicle-operator-licensing.service.gov.uk.";
 
     public static String getConfirmEmailTemppassSentMessageText() throws UninitialisedDriverException {
@@ -21,8 +21,8 @@ public class RegisterConfirmationPage extends BasePage {
         return getText(EMAIL_NOT_RECIVEVED_MESSAGE, SelectorType.CSS);
     }
 
-    public static Pattern checkEmail() {
-        return Pattern.compile(CHECK_EMAIL);
+    public static String checkEmail() {
+        return CHECK_EMAIL;
     }
 
     public static String signingInProblems() {
