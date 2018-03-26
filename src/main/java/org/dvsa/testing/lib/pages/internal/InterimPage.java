1eca99f4047b807cdab145fdb3d7b642c0265da7
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-03-26 11:43:22 +0100

    amendments

--- src/main/java/org/dvsa/testing/lib/pages/internal/InterimPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/internal/InterimPage.java
@@ -8,18 +8,18 @@ public class InterimPage extends BasePage{
 
     //Selectors
     private static String ADD_INTERIM_RADIO_YES = nameAttribute("radio","interimRequested");
-    private static String INTERIM_REASON = "data[interimReason]";
+    private static String INTERIM_REASON_FIELD = "data[interimReason]";
     private static String START_DATE_DAY_FIELD = nameAttribute("input", "data[interimStart][day]");
     private static String START_DATE_MONTH_FIELD = nameAttribute("input", "data[interimStart][month]");
     private static String START_DATE_YEAR_FIELD = nameAttribute("input", "data[interimStart][year]");
     private static String END_DATE_DAY_FIELD = nameAttribute("input", "data[interimEnd][day]");
     private static String END_DATE_MONTH_FIELD = nameAttribute("input", "data[interimEnd][month]");
     private static String END_DATE_YEAR_FIELD = nameAttribute("input", "data[interimEnd][year]");
-    private static String VECHICLE = nameAttribute("input", "data[interimAuthVehicles]");
-    private static String TRAILERS = nameAttribute("input", "data[interimAuthTrailers]");
-    private static String OPERATING_CENTRE = nameAttribute("input", "operatingCentres[id][]");
-    private static String VEHICLE_SELECT = nameAttribute("input", "vehicles[id][]");
-    private static String SAVE = nameAttribute("input", "form-actions[save]");
+    private static String VECHICLE_FIELD = nameAttribute("input", "data[interimAuthVehicles]");
+    private static String TRAILERS_FIELD = nameAttribute("input", "data[interimAuthTrailers]");
+    private static String OPERATING_CENTRE_FIELD = nameAttribute("input", "operatingCentres[id][]");
+    private static String VEHICLE_SELECT_FIELD = nameAttribute("input", "vehicles[id][]");
+    private static String SAVE_FIELD = nameAttribute("input", "form-actions[save]");
 
     //Behaviour
     public static void addInterim() throws UninitialisedDriverException {
@@ -27,7 +27,7 @@ public class InterimPage extends BasePage{
     }
 
     public static void  enterInterimDetails() throws UninitialisedDriverException {
-          enterField(INTERIM_REASON, Str.randomWord(150));
+          enterField(INTERIM_REASON_FIELD, Str.randomWord(150));
     }
 
     public static void startDate(int day, int month, int year) throws UninitialisedDriverException {
@@ -43,22 +43,22 @@ public class InterimPage extends BasePage{
     }
 
     public static void vehicleAuthority(int vehicles) throws UninitialisedDriverException {
-        enterField(VECHICLE, String.valueOf(vehicles));
+        enterField(VECHICLE_FIELD, String.valueOf(vehicles));
     }
 
     public static void trailerAuthority(int trailer) throws UninitialisedDriverException {
-        enterField(TRAILERS, String.valueOf(trailer));
+        enterField(TRAILERS_FIELD, String.valueOf(trailer));
     }
 
     public static void operatingCentre() throws UninitialisedDriverException {
-        click(OPERATING_CENTRE);
+        click(OPERATING_CENTRE_FIELD);
     }
 
     public static void vehicleSelect() throws UninitialisedDriverException {
-        click(VEHICLE_SELECT);
+        click(VEHICLE_SELECT_FIELD);
     }
 
     public static void save() throws UninitialisedDriverException {
-        click(SAVE);
+        click(SAVE_FIELD);
     }
 }
