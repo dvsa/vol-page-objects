e9c122d3aba913f8cca9105359738944a05e9ea9
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-03-26 08:42:29 +0100

    Applcation Page objects page

---
+++ src/main/java/org/dvsa/testing/lib/pages/internal/Application.java
@@ -0,0 +1,17 @@
+package org.dvsa.testing.lib.pages.internal;
+
+import org.dvsa.testing.lib.browser.exceptions.UninitialisedDriverException;
+import org.dvsa.testing.lib.pages.BasePage;
+import activesupport.string.Str;
+import org.dvsa.testing.lib.browser.exceptions.UninitialisedDriverException;
+import org.dvsa.testing.lib.pages.enums.SelectorType;
+
+public class Application extends BasePage {
+
+
+        public static String INTERIM_LINK = "//*/a[contains(text(), 'Interim details')]";
+
+        private static void interimLink() throws UninitialisedDriverException {
+            click(INTERIM_LINK);
+        }
+    }
