33e570734beba7d190d10d6a4de4322dc3a8301e
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-06-04 10:12:02 +0100

    Create page object for 'Nature of your business' page

---
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/NatureOfBusinessPage.java
@@ -0,0 +1,38 @@
+package org.dvsa.testing.lib.pages.external.permit;
+
+import org.dvsa.testing.lib.pages.BasePage;
+import org.dvsa.testing.lib.pages.enums.SelectorType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Stream;
+
+public class NatureOfBusinessPage extends BasePage {
+    // TODO: fill in identifiers once devs complete pages
+    private static String TYPE_OF_GOOD_TEMPLATE;
+
+    private static String SAVE_AND_CONTINUE_BUTTON;
+    private static String CANCEL_BUTTON;
+
+    public static void typeOfGoods(@NotNull GoodType... goods) {
+        Stream.of(goods).forEach((good) -> {
+            String selector = String.format(TYPE_OF_GOOD_TEMPLATE, good.toString());
+
+            scrollTo(
+                    String.format(TYPE_OF_GOOD_TEMPLATE, good.toString()),
+                    SelectorType.XPATH,
+                    BasePage.WAIT_TIME_SECONDS
+            );
+
+            click(selector);
+        });
+    }
+
+    public static void saveAndContinue() {
+        click(SAVE_AND_CONTINUE_BUTTON);
+    }
+
+    public static void cancel() {
+        click(CANCEL_BUTTON);
+    }
+
+}
