ab7956d652904425a9ef283d8ff2a939c2e22382
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-06-18 11:52:26 +0100

    Assign selector fields in VehicleStandardPage with values

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/ECMTRestrictionsPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/ECMTRestrictionsPage.java
@@ -7,8 +7,8 @@ public class ECMTRestrictionsPage extends BasePage {
     // TODO: fill in identifiers once devs complete pages
     private static String ECMT_RESTRICTED_COUNTRY_TEMPLATE = "#restricted-countries label:nth-of-type(%d) input[type='radio']";
 
-    private static String SAVE_AND_CONTINUE_BUTTON = "input[type='submit']";
-    private static String CANCEL_BUTTON = "//a[contains(text(), 'Cancel')]";
+    private static String SAVE_AND_CONTINUE = "input[type='submit']";
+    private static String SAVE_AND_RETURN = "//a[contains(text(), 'Cancel')]";
 
     public static void deliverToRestrictedCountry(boolean answer) {
         int position = answer ? 1 : 2;
@@ -16,11 +16,11 @@ public class ECMTRestrictionsPage extends BasePage {
     }
 
     public static void saveAndContinue() {
-        scrollAndClick(SAVE_AND_CONTINUE_BUTTON);
+        scrollAndClick(SAVE_AND_CONTINUE);
     }
 
-    public static void cancel() {
-        scrollAndClick(CANCEL_BUTTON, SelectorType.XPATH);
+    public static void saveAndReturn() {
+        scrollAndClick(SAVE_AND_RETURN, SelectorType.XPATH);
     }
 
 }
--- src/main/java/org/dvsa/testing/lib/pages/external/permit/VehicleStandardPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/VehicleStandardPage.java
@@ -1,13 +1,14 @@
 package org.dvsa.testing.lib.pages.external.permit;
 
 import org.dvsa.testing.lib.pages.BasePage;
+import org.dvsa.testing.lib.pages.enums.SelectorType;
 
 public class VehicleStandardPage extends BasePage {
 
-    private static String IS_EURO_6_COMPLIANT_TEMPLATE;
+    private static String IS_EURO_6_COMPLIANT_TEMPLATE = "#euro6 label:nth-of-type(%d) input[type='radio']";
 
-    private static String SAVE_AND_CONTINUE;
-    private static String SAVE_AND_RETURN;
+    private static String SAVE_AND_CONTINUE = "input[type='submit']";
+    private static String SAVE_AND_RETURN = "//a[contains(text(), 'Cancel')]";
 
     public static void isEuro6Compliant(boolean isCompliant) {
         int pos = isCompliant ? 1 : 2;
@@ -19,7 +20,7 @@ public class VehicleStandardPage extends BasePage {
     }
 
     public static void saveAndReturn() {
-        scrollAndClick(SAVE_AND_RETURN);
+        scrollAndClick(SAVE_AND_RETURN, SelectorType.XPATH);
     }
 
 }
