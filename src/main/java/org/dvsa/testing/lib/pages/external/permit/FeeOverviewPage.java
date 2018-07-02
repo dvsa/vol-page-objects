d36357453cce922df71a7159aa053ebc1fdba3fe
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-07-02 14:07:22 +0100

    Assign selector values to FeeOverviewPage page object

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/FeeOverviewPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/FeeOverviewPage.java
@@ -1,18 +1,19 @@
 package org.dvsa.testing.lib.pages.external.permit;
 
 import org.dvsa.testing.lib.pages.BasePage;
+import org.dvsa.testing.lib.pages.enums.SelectorType;
 
 public class FeeOverviewPage extends BasePage {
 
-    private static String SUBMIT_AND_PAY;
-    private static String SAVE_AND_RETURN;
+    private static String SUBMIT_AND_PAY = "//a[text()='Submit and pay']";
+    private static String SAVE_AND_RETURN = "//a[contains(text(), 'Save and return to permits')]" ;
 
     public static void submitAndPay() {
-        scrollAndClick(SUBMIT_AND_PAY);
+        scrollAndClick(SUBMIT_AND_PAY, SelectorType.XPATH);
     }
 
     public static void saveAndReturn() {
-        scrollAndClick(SAVE_AND_RETURN);
+        scrollAndClick(SAVE_AND_RETURN, SelectorType.XPATH);
     }
 
 }
