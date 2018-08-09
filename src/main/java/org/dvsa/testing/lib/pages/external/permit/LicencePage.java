3e91215c94663ed9fe7790ab8e30fb4e35ea9c94
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-08-09 10:14:25 +0100

    Create LicencePage page object

---
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/LicencePage.java
@@ -0,0 +1,35 @@
+package org.dvsa.testing.lib.pages.external.permit;
+
+import activesupport.string.Str;
+import org.dvsa.testing.lib.pages.BasePage;
+
+public class LicencePage extends BasePage {
+    // TODO: Assign selectors when their values are defined
+    private static String TITLE = "h1";
+    private static String LICENCE_INDEX_TEMPLATE = "label:nth-of-type(%d) input[type=radio]";
+    private static String NUM_LICENCE = "label input[type=radio]";
+    private static String SELECTED_LICENCE = "label[class*='selected'] input[type=radio]";
+
+    private static String LICENCE_REGEX = "(?<=Permit application for licence )\\w{9}";
+
+    public static boolean hasSelectedLicence(){
+        return isElementPresent(SELECTED_LICENCE);
+    }
+
+    public static boolean hasNoSelectedLicence(){
+        return !hasSelectedLicence();
+    }
+
+    public static String getLicenceNumber(){
+        return Str.find(LICENCE_REGEX, getText(TITLE));
+    }
+
+    public static int numOfLicences(){
+        return size(NUM_LICENCE);
+    }
+
+    public static void licence(int index){
+        scrollAndClick(String.format(LICENCE_INDEX_TEMPLATE, index));
+    }
+
+}
