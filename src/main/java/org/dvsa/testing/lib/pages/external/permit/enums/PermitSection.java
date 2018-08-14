b87965df4c06a1e22ab66df36645f53dbd6b1a43
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-08-14 16:40:50 +0100

    Create OverviewPage#section to select the different sections shown on the overview page

---
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/enums/PermitSection.java
@@ -0,0 +1,27 @@
+package org.dvsa.testing.lib.pages.external.permit.enums;
+
+public enum PermitSection {
+
+    LicenceNumber("Licence Number"),
+    EuroEmmistionsStandards("Euro emission standards"),
+    Cabotage("Cabotage"),
+    GoodsToLimitedCountries("Goods to limited countries"),
+    NumberOfPermitsRequired("Number of permits required"),
+    AnnualTripsAbroad("Annual trips abroad"),
+    PercentageOfInternationalJourneys("Percentage of international journeys"),
+    GoodsYouCarryAbroad("Goods you carry abroad"),
+    CheckYourAnswers("Check your answers"),
+    Declaration("Declaration");
+
+    private String section;
+
+    PermitSection(String section){
+        this.section = section;
+    }
+
+    @Override
+    public String toString() {
+        return section;
+    }
+
+}
