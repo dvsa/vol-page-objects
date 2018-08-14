6c7136d56524d44a34fb2f1683f72d39bc7a5f51
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-08-14 17:12:42 +0100

    Rewrite VehicleStandardPage#isEuro6Compliant and extend BasePermitPage so that cancel and saveAndContinue could be inherited and delete their declarations within VehicleStandardPage class

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/VehicleStandardPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/VehicleStandardPage.java
@@ -1,26 +1,18 @@
 package org.dvsa.testing.lib.pages.external.permit;
 
-import org.dvsa.testing.lib.pages.BasePage;
-import org.dvsa.testing.lib.pages.enums.SelectorType;
+public class VehicleStandardPage extends BasePermitPage {
 
-public class VehicleStandardPage extends BasePage {
-
-    private static String IS_EURO_6_COMPLIANT_TEMPLATE = "#euro6 label:nth-of-type(%d) input[type='radio']";
-
-    private static String SAVE_AND_CONTINUE = "input[type='submit']";
-    private static String SAVE_AND_RETURN = "//a[contains(text(), 'Save and return to permits')]";
+    private static String CONFIRM_COMPLIANCE = "#MeetsEuro6";
+    private static String IS_COMPLIANT = "label.selected " + CONFIRM_COMPLIANCE;
 
     public static void isEuro6Compliant(boolean isCompliant) {
-        int pos = isCompliant ? 1 : 2;
-        scrollAndClick(String.format(IS_EURO_6_COMPLIANT_TEMPLATE, pos));
-    }
-
-    public static void saveAndContinue() {
-        scrollAndClick(SAVE_AND_CONTINUE);
-    }
-
-    public static void saveAndReturn() {
-        scrollAndClick(SAVE_AND_RETURN, SelectorType.XPATH);
+        if (isCompliant){
+            if (isElementNotPresent(IS_COMPLIANT)) // Checks to see if the checkbox is already selected
+                scrollAndClick(CONFIRM_COMPLIANCE);
+        }  else {
+            if (isElementPresent(IS_COMPLIANT))
+                scrollAndClick(CONFIRM_COMPLIANCE);
+        }
     }
 
 }
