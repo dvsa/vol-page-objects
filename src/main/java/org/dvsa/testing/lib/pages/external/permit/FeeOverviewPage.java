7332ee56706fe626bd48c392f0554f3be3a76ab2
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-08-15 12:51:42 +0100

    Update FeeOverviewPage to inherit form BasePermitPage and rename #submitAndPay to #saveAndContinue and #saveAndReturn to #overview

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/FeeOverviewPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/FeeOverviewPage.java
@@ -1,19 +1,16 @@
 package org.dvsa.testing.lib.pages.external.permit;
 
-import org.dvsa.testing.lib.pages.BasePage;
-import org.dvsa.testing.lib.pages.enums.SelectorType;
+public class FeeOverviewPage extends BasePermitPage {
 
-public class FeeOverviewPage extends BasePage {
+    private static String SUBMIT_AND_PAY = "#submit-accept-button";
+    private static String OVERVIEW = ".ecmt-overview-return-link";
 
-    private static String SUBMIT_AND_PAY = "//a[text()='Submit and pay']";
-    private static String SAVE_AND_RETURN = "//a[contains(text(), 'Save and return to permits')]" ;
-
-    public static void submitAndPay() {
-        scrollAndClick(SUBMIT_AND_PAY, SelectorType.XPATH);
+    public static void saveAndContinue() {
+        scrollAndClick(SUBMIT_AND_PAY);
     }
 
-    public static void saveAndReturn() {
-        scrollAndClick(SAVE_AND_RETURN, SelectorType.XPATH);
+    public static void overview(){
+        scrollAndClick(OVERVIEW);
     }
 
 }
