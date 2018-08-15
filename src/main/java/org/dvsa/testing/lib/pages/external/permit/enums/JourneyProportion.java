34867dc126705abcc5092a394e777f9026a08b70
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-08-15 10:52:41 +0100

    Create PercentageOfInternationalJourneysPage page object

---
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/enums/JourneyProportion.java
@@ -0,0 +1,26 @@
+package org.dvsa.testing.lib.pages.external.permit.enums;
+
+import activesupport.number.Int;
+
+public enum JourneyProportion {
+
+    LessThan60Percent(1),
+    From60To90Percent(2),
+    MoreThan90Percent(3);
+
+    private int index;
+
+    JourneyProportion(int index){
+        this.index = index;
+    }
+
+    public static JourneyProportion random(){
+        int index = Int.random(0, JourneyProportion.values().length);
+        return JourneyProportion.values()[index];
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(index);
+    }
+}
