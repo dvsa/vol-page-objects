56404b3ad546ce36058b803f4c0a6f1edb144cd4
Tamilarasi Kannappan <tamilarasi.kannappan@capgemini.com>
2018-08-22 17:03:12 +0100

    added resource variable

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
@@ -6,6 +6,8 @@ public class ConfirmationPage extends BasePage {
 
     private static String CONTINUE;
 
+    final public static String RESOURCE = "permits/114/ecmt-submitted";
+
     public static void continueButton() {
         scrollAndClick(CONTINUE);
     }
