d76d33a48527d5bccd3ade1586dfb06f0cef3b85
Tamilarasi Kannappan <tamilarasi.kannappan@capgemini.com>
2018-08-23 09:13:25 +0100

    changed the path for the Resource variable

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
@@ -6,7 +6,7 @@ public class ConfirmationPage extends BasePage {
 
     private static String CONTINUE;
 
-    final public static String RESOURCE = "permits/114/ecmt-submitted";
+    final public static String RESOURCE = "/ecmt-submitted";
 
     public static void continueButton() {
         scrollAndClick(CONTINUE);
