f73372f49cbfa574e6877d313be5e6a5a3e83a48
Tamilarasi Kannappan <tamilarasi.kannappan@capgemini.com>
2018-09-03 14:25:22 +0100

    added step definition for confirmation page

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
@@ -1,15 +1,24 @@
 package org.dvsa.testing.lib.pages.external.permit;
 
 import org.dvsa.testing.lib.pages.BasePage;
+import org.dvsa.testing.lib.pages.enums.SelectorType;
 
 public class ConfirmationPage extends BasePage {
 
-    private static String CONTINUE;
+    private static String FINISH = "//a[contains(text(), 'Finish')]";
+    private static String VIEWRECEIPT = "//a[contains(text(), 'View Receipt')]";
 
     final public static String RESOURCE = "/ecmt-submitted";
 
-    public static void continueButton() {
-        scrollAndClick(CONTINUE);
+    final public static String DASHBOARD = "/permits";
+
+    final public static String PAYMENTPAGE = "/payment_receipt.html";
+
+    public static void finishButton() {
+        scrollAndClick(FINISH, SelectorType.XPATH);
     }
+
+    public static void ViewReceiptLink()
+    {scrollAndClick(VIEWRECEIPT,SelectorType.XPATH);}
 
 }
