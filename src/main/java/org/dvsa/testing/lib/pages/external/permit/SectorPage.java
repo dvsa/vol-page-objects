48523708ab3e106e026420e5e909e396a4af28fa
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-07 15:59:35 +0100

    Update SectorPage to reflect changes made in iteration 10 of ECMT

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/SectorPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/SectorPage.java
@@ -4,13 +4,8 @@ import org.dvsa.testing.lib.pages.external.permit.enums.Sector;
 
 public class SectorPage extends BasePermitPage {
 
-    private static String SPECIALISES_TEMPLATE = "fieldset[data-group='Fields'] > label:nth-of-type(%d)";
-    private static String SECTOR_TEMPLATE = ".sector-list label:nth-of-type(%d) input[type='radio']";
+    private static String SECTOR_TEMPLATE = "#SpecialistHaulage label:nth-of-type(%d) input[type=radio]";
 
-    public static void specialist(boolean specialist){
-        int index = specialist ? 1 : 2;
-        scrollAndClick(String.format(SPECIALISES_TEMPLATE, index));
-    }
 
     public static void sector(Sector sector){
         scrollAndClick(String.format(SECTOR_TEMPLATE, sector.ordinal() + 1));
