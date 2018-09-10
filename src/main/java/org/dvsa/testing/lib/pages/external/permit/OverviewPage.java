6aa35ddbc99402da79ee09894dc22b82d79b3743
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-10 11:09:21 +0100

    Create OverviewPage#statusOfSection which retrieves the status of the specified section.

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/OverviewPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/OverviewPage.java
@@ -9,17 +9,32 @@ import org.jetbrains.annotations.NotNull;
 public class OverviewPage extends BasePermitPage {
 
     private static String SECTION_TEMPLATE = "//a/span[contains(text(), '%s')]/..";
+    private static String ACTIVE_SECTION_STATUS_TEMPLATE = "//span[contains(text(), '%s')]/following-sibling::span";
+    private static String DISABLED_SECTION_STATUS_TEMPLATE = "//div[contains(text(), '%s')]/span";
 
     final public static String RESOURCE = "/application-overview";
-    private static String OVERVIEWPAGE_STATUS_TEMPLATE = "//span[contains(text(), '%s')]/following-sibling::span";
 
     public static void section(@NotNull PermitSection section){
         scrollAndClick(String.format(SECTION_TEMPLATE, section.toString()), SelectorType.XPATH);
     }
 
     public static boolean checkStatus(PermitSection section, PermitStatus status){
-        String sectionStatus = getText(String.format(OVERVIEWPAGE_STATUS_TEMPLATE, section.toString()), SelectorType.XPATH);
+        String sectionStatus = getText(String.format(ACTIVE_SECTION_STATUS_TEMPLATE, section.toString()), SelectorType.XPATH);
         return StringUtils.trim(sectionStatus).toLowerCase().contains(StringUtils.trim(status.toString()).toLowerCase());
     }
 
+    public static PermitStatus statusOfSection(@NotNull PermitSection section) {
+        String selector = sectionState(section) == State.ACTIVE ? ACTIVE_SECTION_STATUS_TEMPLATE : DISABLED_SECTION_STATUS_TEMPLATE;
+        String status = getText(String.format(selector, section.toString()), SelectorType.XPATH);
+        return PermitStatus.getEnum(status);
+    }
+
+    private static State sectionState(@NotNull PermitSection section){
+        return isElementPresent(String.format(ACTIVE_SECTION_STATUS_TEMPLATE, section.toString()), SelectorType.XPATH) ? State.ACTIVE : State.DISABLED;
+    }
+
+    private enum State {
+        ACTIVE,
+        DISABLED
+    }
 }
