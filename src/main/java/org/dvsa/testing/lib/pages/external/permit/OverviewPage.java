ebf46cf333b6b5cf07652dbb1a4b008f7ffd0980
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-10 16:46:28 +0100

    Implement OverviewPage#numOfCompleteSections which returns the nimber of completed sections from within the progress panel

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/OverviewPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/OverviewPage.java
@@ -1,5 +1,6 @@
 package org.dvsa.testing.lib.pages.external.permit;
 
+import activesupport.string.Str;
 import com.amazonaws.util.StringUtils;
 import org.dvsa.testing.lib.enums.PermitStatus;
 import org.dvsa.testing.lib.pages.enums.SelectorType;
@@ -11,6 +12,8 @@ public class OverviewPage extends BasePermitPage {
     private static String SECTION_TEMPLATE = "//a/span[contains(text(), '%s')]/..";
     private static String ACTIVE_SECTION_STATUS_TEMPLATE = "//span[contains(text(), '%s')]/following-sibling::span";
     private static String DISABLED_SECTION_STATUS_TEMPLATE = "//div[contains(text(), '%s')]/span";
+
+    private static String PROGRESS_PANEL_COMPLETE_SECTIONS = ".sidebar p:nth-of-type(1) b";
 
     final public static String RESOURCE = "/application-overview";
 
@@ -32,5 +35,10 @@ public class OverviewPage extends BasePermitPage {
 
     public static boolean isActive(@NotNull PermitSection section){
         return isElementPresent(String.format(ACTIVE_SECTION_STATUS_TEMPLATE, section.toString()), SelectorType.XPATH);
+    }
+
+    public static int numOfCompleteSections() {
+        String subject = getText(PROGRESS_PANEL_COMPLETE_SECTIONS);
+        return Integer.parseInt(Str.find("\\d(?= of \\d{1,2} complete)", subject));
     }
 }
