dd3189d70926298f88dd960f199fce556ef353a8
Tamilarasi Kannappan <tamilarasi.kannappan@capgemini.com>
2018-09-14 13:25:40 +0100

    Added a variable for accept_and_continue button

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/ConfirmationPage.java
@@ -7,18 +7,22 @@ public class ConfirmationPage extends BasePage {
 
     private static String FINISH = "//a[contains(text(), 'Finish')]";
     private static String VIEWRECEIPT = "//a[contains(text(), 'View Receipt')]";
+    private static String ACCEPT_AND_CONTINUE = "//*[@id='submit-accept-button']";
 
-    final public static String RESOURCE = "/ecmt-submitted";
+    final public static String RESOURCE = "/ecmt-submitted/";
 
-    final public static String DASHBOARD = "/permits";
+    final public static String DASHBOARD = "permits";
 
-    final public static String PAYMENTPAGE = "/payment_receipt.html";
+    final public static String PAYMENTPAGE = "payment_receipt.html";
 
     public static void finishButton() {
         scrollAndClick(FINISH, SelectorType.XPATH);
     }
 
-    public static void ViewReceiptLink()
+    public static void viewReceiptLink()
     {scrollAndClick(VIEWRECEIPT,SelectorType.XPATH);}
 
+    public static void acceptAndContinueButton() {
+        scrollAndClick(ACCEPT_AND_CONTINUE,SelectorType.XPATH);
+    }
 }
