7f9f601b9f17cf7cd4d536f365c6708c8c305508
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-20 15:36:23 +0100

    FeeOverviewPage#reference which returns the application reference number from the fee overview page

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/FeeOverviewPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/FeeOverviewPage.java
@@ -2,6 +2,8 @@ package org.dvsa.testing.lib.pages.external.permit;
 
 public class FeeOverviewPage extends BasePermitPage {
 
+    private static String REFERENCE_NUMBER = "tbody tr:first-child td:nth-of-type(2)";
+
     private static String SUBMIT_AND_PAY = "#submit-accept-button";
     private static String OVERVIEW = ".ecmt-overview-return-link";
 
@@ -11,6 +13,10 @@ public class FeeOverviewPage extends BasePermitPage {
 
     public static void overview(){
         scrollAndClick(OVERVIEW);
+    }
+
+    public static String reference() {
+        return getText(REFERENCE_NUMBER);
     }
 
 }
