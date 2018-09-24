7d5a5b36e1da426f97887ca4a0055f771127735c
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-24 16:30:51 +0100

    Rename ApplicationOverviewPage to CheckYourAnswersPage and also delete CheckAnswersPage as it effectively a duplicate of CheckYourAnswersPage

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/CheckYourAnswersPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/CheckYourAnswersPage.java
@@ -5,14 +5,12 @@ import org.dvsa.testing.lib.pages.enums.SelectorType;
 import org.dvsa.testing.lib.pages.external.permit.enums.ApplicationInfo;
 import org.jetbrains.annotations.NotNull;
 
-public class ApplicationOverviewPage extends BasePage {
-    // TODO: fill in identifiers once devs complete pages
+public class CheckYourAnswersPage extends BasePermitPage {
     private static String ANSWER_ROW_TEMPLATE = "tbody tr:nth-of-type(%d) ";
 
     private static String ANSWER_VALUE_DATA_CELL = " td:nth-of-type(2)";
 
-    private static String ACCEPT_AND_CONTINUE_BUTTON = "//a[contains(text(), 'Accept and continue')]";
-    private static String SAVE_AND_RETURN = "//a[contains(text(), 'Save and return to permits')]";
+    private static String CONFIRM_AND_CONTINUE_BUTTON = "#submit-accept-button";
 
     public static String getAnswer(@NotNull ApplicationInfo info) {
         String selector = String.format(ANSWER_ROW_TEMPLATE, info.ordinal() + 1);
@@ -23,11 +21,7 @@ public class ApplicationOverviewPage extends BasePage {
     }
 
     public static void acceptAndContinue() {
-        scrollAndClick(ACCEPT_AND_CONTINUE_BUTTON, SelectorType.XPATH);
-    }
-
-    public static void SaveAndReturn() {
-        scrollAndClick(SAVE_AND_RETURN);
+        scrollAndClick(CONFIRM_AND_CONTINUE_BUTTON, SelectorType.XPATH);
     }
 
 }
