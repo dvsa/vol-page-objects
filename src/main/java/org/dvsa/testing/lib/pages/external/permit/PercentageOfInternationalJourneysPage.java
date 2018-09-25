29991dc89b5c732c603162c3f9fc2e5add7e6c35
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-25 14:03:50 +0100

    Update PercentageOfInternationalPage#RESOURCE value and add URL wait to PercentageOfInternationalPage#proportion

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/PercentageOfInternationalJourneysPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/PercentageOfInternationalJourneysPage.java
@@ -1,5 +1,6 @@
 package org.dvsa.testing.lib.pages.external.permit;
 
+import org.dvsa.testing.lib.browser.Browser;
 import org.dvsa.testing.lib.pages.enums.SelectorType;
 import org.dvsa.testing.lib.pages.external.permit.enums.JourneyProportion;
 
@@ -9,9 +10,10 @@ public class PercentageOfInternationalJourneysPage extends BasePermitPage {
 
     private static String PROPORTION_TEMPLATE = "label:nth-of-type(%s) input[type=radio]";
 
-    final public static String RESOURCE = "ecmt-international-journey/";
+    final public static String RESOURCE = "ecmt-trips/";
 
     public static void proportion(JourneyProportion proportion){
+        Browser.Wait.untilUrlIs(RESOURCE, TimeUnit.SECONDS, BasePermitPage.WAIT_TIME_SECONDS);
         String selector = String.format(PROPORTION_TEMPLATE, proportion.ordinal() + 1);
 
         untilElementIsPresent(selector, SelectorType.CSS, BasePermitPage.WAIT_TIME_SECONDS, TimeUnit.SECONDS);
