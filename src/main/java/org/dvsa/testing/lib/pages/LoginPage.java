6ae8414e4a2565c432401a0eeebcc679ae74698e
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-26 10:02:15 +0100

    Update LoginPage locators

--- src/main/java/org/dvsa/testing/lib/pages/LoginPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/LoginPage.java
@@ -5,9 +5,9 @@ import org.jetbrains.annotations.NotNull;
 public class LoginPage extends BasePage {
 
     // Selectors
-    private static String EMAIL_FIELD_LOCATOR = nameAttribute("input", "username");
-    private static String PASSWORD_FIELD_LOCATOR = nameAttribute("input", "password");
-    private static String SUBMIT_BUTTON = nameAttribute("input", "submit") + "[value=\"Sign in\"]";
+    private static String USERNAME_FIELD_LOCATOR = "input[name='username']";
+    private static String PASSWORD_FIELD_LOCATOR = "input[name='password']";
+    private static String SUBMIT_BUTTON = "input[name='submit']";
 
     // Attributes
     private static String RESOURCE_PATH = "auth/login/";
@@ -18,15 +18,15 @@ public class LoginPage extends BasePage {
 
     // Behaviour
     public static void username(@NotNull String username) {
-        enterField(EMAIL_FIELD_LOCATOR, username);
+        scrollAndEnterField(USERNAME_FIELD_LOCATOR, username);
     }
 
     public static void password(@NotNull String password) {
-        enterField(PASSWORD_FIELD_LOCATOR, password);
+        scrollAndEnterField(PASSWORD_FIELD_LOCATOR, password);
     }
 
     public static void submit() {
-        click(SUBMIT_BUTTON);
+        scrollAndClick(SUBMIT_BUTTON);
     }
 
     public static void signIn(String username, String password) {
