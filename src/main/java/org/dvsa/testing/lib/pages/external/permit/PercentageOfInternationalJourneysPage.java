9b5d7ec451a1a4ebc6f38ba7ee344e07febfdfe3
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-10-02 16:58:07 +0100

    Create PercentageOfInternationalJourneysPage#hasIntensityMessage which returns true if the current page has the intensity of use message

--- src/main/java/org/dvsa/testing/lib/pages/external/permit/PercentageOfInternationalJourneysPage.java
+++ src/main/java/org/dvsa/testing/lib/pages/external/permit/PercentageOfInternationalJourneysPage.java
@@ -12,12 +12,18 @@ public class PercentageOfInternationalJourneysPage extends BasePermitPage {
 
     final public static String RESOURCE = "ecmt-international-journey/";
 
+    private static String INTENSITY_MESSAGE = "You have stated a high percentage of international journeys for this licence. Please check the details are correct. We may contact you to verify this information";
+
     public static void proportion(JourneyProportion proportion){
         Browser.Wait.untilUrlPathIs(RESOURCE, TimeUnit.SECONDS, BasePermitPage.WAIT_TIME_SECONDS);
         String selector = String.format(PROPORTION_TEMPLATE, proportion.ordinal() + 1);
 
         untilElementIsPresent(selector, SelectorType.CSS, BasePermitPage.WAIT_TIME_SECONDS, TimeUnit.SECONDS);
         scrollAndClick(String.format(PROPORTION_TEMPLATE, proportion.ordinal() + 1));
+    }
+
+    public static boolean hasIntensityMessage() {
+        return isTextPresent(INTENSITY_MESSAGE);
     }
 
 }
